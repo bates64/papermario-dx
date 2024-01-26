dd4ae57be14a94bf12e04eb4e5082f102c6fefca
Alex Bates <alex@nanaian.town>
2024-01-26 23:20:49 +0000

    boot into tst_13

--- src/dx/config.h
+++ src/dx/config.h
@@ -5,7 +5,7 @@
 #define DX_DEBUG_MENU 1
 
 /// Loads last used save file
-#define DX_QUICK_LAUNCH 0
+#define DX_QUICK_LAUNCH 1
 
 /// Skips logos (Nintendo, Intelligent Systems, etc.).
 #define DX_SKIP_LOGOS 1
@@ -17,10 +17,10 @@
 #define DX_SKIP_DEMO 1
 
 /// Map to load when starting a new game.
-#define NEW_GAME_MAP_ID "nok_11"
+#define NEW_GAME_MAP_ID "tst_13"
 
 /// Entry to use when starting a new game.
-#define NEW_GAME_ENTRY_ID nok_11_ENTRY_0
+#define NEW_GAME_ENTRY_ID tst_13_ENTRY_0
 
 /// Story progress when starting a new game.
 #define NEW_GAME_STORY_PROGRESS STORY_CH1_MADE_FIRST_BRIDGE
