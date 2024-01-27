2e6123bb9d8cc7d1475c4ead3a744970d1cae95c
HailSanta <Hail2Santa@gmail.com>
2024-01-27 00:21:27 -0500

    remove old menu

--- src/dx/old_debug_menu.h
+++
@@ -1,9 +0,0 @@
-#include "common.h"
-#include "dx/config.h"
-
-void dx_debug_menu_push(void);
-void dx_debug_menu_pop(void);
-void dx_debug_menu_close(void);
-void dx_render_debug_menu(void);
-
-u8* dx_old_string_to_msg(u8* msg, const u8* str);
