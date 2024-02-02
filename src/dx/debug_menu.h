c12f7cd6455186ebf004cb67d05de1d374cb12cb
Alex Bates <alex@nanaian.town>
2024-02-02 12:27:56 +0000

    add DX_QUICK_LAUNCH_BATTLE

--- src/dx/debug_menu.h
+++ src/dx/debug_menu.h
@@ -1,6 +1,6 @@
 #include "common.h"
 #include "dx/config.h"
-#if DX_DEBUG_MENU
+#if DX_DEBUG_MENU || defined(DX_QUICK_LAUNCH_BATTLE)
 
 #define DX_DEBUG_DUMMY_ID 0xDEAD
 
@@ -13,6 +13,8 @@ b32 dx_debug_should_hide_models();
 
 void dx_debug_set_map_info(char* mapName, s32 entryID);
 void dx_debug_set_battle_info(s32 battleID, char* stageName);
+
+void dx_debug_begin_battle_with_IDs(s16 battle, s16 stage);
 
 #define debug_print(text) dx_hashed_debug_printf(__FILE__,__LINE__,text)
 #define debug_printf(fmt, args...) dx_hashed_debug_printf(__FILE__,__LINE__,fmt,args)
