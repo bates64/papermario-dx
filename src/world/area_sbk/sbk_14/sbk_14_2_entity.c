8b31e548bb2cf24cc8c0d9ecb2b87d6927e05b53
HailSanta <Hail2Santa@gmail.com>
2024-02-26 17:33:08 -0500

    tweester names

--- src/world/area_sbk/sbk_14/sbk_14_2_entity.c
+++ src/world/area_sbk/sbk_14/sbk_14_2_entity.c
@@ -1,7 +1,7 @@
 #include "sbk_14.h"
 #include "entity.h"
 
-TweesterPath N(D_80240440_936B20) = {
+TweesterPath N(DefaultTweesterPath) = {
     -65, 0, 217,
     -262, 0, -64,
     57, 0, -286,
@@ -9,12 +9,12 @@ TweesterPath N(D_80240440_936B20) = {
     TWEESTER_PATH_LOOP
 };
 
-TweesterPath* N(D_80240474_936B54)[] = {
-    &N(D_80240440_936B20),
+TweesterPath* N(TweesterPaths)[] = {
+    &N(DefaultTweesterPath),
     PTR_LIST_END
 };
 
-EvtScript N(D_8024047C_936B5C) = {
+EvtScript N(EVS_GotoMap_sbk_05_4) = {
     Call(DisablePlayerInput, TRUE)
     Call(DisablePlayerPhysics, TRUE)
     Call(GotoMap, Ref("sbk_05"), sbk_05_ENTRY_4)
@@ -24,8 +24,8 @@ EvtScript N(D_8024047C_936B5C) = {
 };
 
 EvtScript N(EVS_MakeEntities) = {
-    Call(MakeEntity, Ref(Entity_Tweester), 327, 0, 8, 0, Ref(N(D_80240474_936B54)), MAKE_ENTITY_END)
-    Call(AssignScript, Ref(N(D_8024047C_936B5C)))
+    Call(MakeEntity, Ref(Entity_Tweester), 327, 0, 8, 0, Ref(N(TweesterPaths)), MAKE_ENTITY_END)
+    Call(AssignScript, Ref(N(EVS_GotoMap_sbk_05_4)))
     Call(MakeEntity, Ref(Entity_MulticoinBlock), 0, 60, -100, 0, MAKE_ENTITY_END)
     Call(AssignBlockFlag, GF_SBK14_MultiCoinBrick)
     Call(MakeEntity, Ref(Entity_YellowBlock), -240, 0, 215, 0, ITEM_COIN, MAKE_ENTITY_END)
