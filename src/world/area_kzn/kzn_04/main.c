ed1ccac746326aa1388eb8b3cad9ff3c9444972e
Alex Bates <alex@bates64.com>
2024-06-30 20:33:38 +0100

    fix conflict in kzn_04

--- src/world/area_kzn/kzn_04/main.c
+++ src/world/area_kzn/kzn_04/main.c
@@ -1,10 +1,6 @@
 #include "kzn_04.h"
 
-<<<<<<< HEAD
-#include "world/common/atomic/TexturePan.inc.c"
-=======
 #include "../common/SmokeTexPanners.inc.c"
->>>>>>> main
 
 EvtScript N(EVS_ExitWalk_kzn_03_1) = EVT_EXIT_WALK(60, kzn_04_ENTRY_0, "kzn_03", kzn_03_ENTRY_1);
 
@@ -14,39 +10,10 @@ EvtScript N(EVS_BindExitTriggers) = {
     End
 };
 
-<<<<<<< HEAD
-EvtScript N(EVS_StartTexPanners) = {
-    // smoke
-    Call(SetTexPanner, MODEL_kem1, TEX_PANNER_3)
-    Thread
-        TEX_PAN_PARAMS_ID(TEX_PANNER_3)
-        TEX_PAN_PARAMS_STEP( -200,    0,  600, -400)
-        TEX_PAN_PARAMS_FREQ(    1,    0,    1,    1)
-        TEX_PAN_PARAMS_INIT(    0,    0,    0,    0)
-        Exec(N(EVS_UpdateTexturePan))
-    EndThread
-    Call(SetTexPanner, MODEL_kem2, TEX_PANNER_4)
-    Thread
-        TEX_PAN_PARAMS_ID(TEX_PANNER_4)
-        TEX_PAN_PARAMS_STEP( 500,    0,    0, -400)
-        TEX_PAN_PARAMS_FREQ(   1,    0,    0,    1)
-        TEX_PAN_PARAMS_INIT(   0,    0,    0,    0)
-        Exec(N(EVS_UpdateTexturePan))
-    EndThread
-    Return
-    End
-};
-
 EvtScript N(EVS_Main) = {
     Set(GB_WorldLocation, LOCATION_MT_LAVALAVA)
     Call(SetSpriteShading, SHADING_KZN_04)
     EVT_SETUP_CAMERA_DEFAULT(0, 0, 0)
-=======
-EvtScript N(EVS_Main) = {
-    Set(GB_WorldLocation, LOCATION_MT_LAVALAVA)
-    Call(SetSpriteShading, SHADING_KZN_04)
-    EVT_SETUP_CAMERA_DEFAULT()
->>>>>>> main
     Call(MakeNpcs, TRUE, Ref(N(DefaultNPCs)))
     ExecWait(N(EVS_MakeEntities))
     Set(LVar0, Ref(N(EVS_BindExitTriggers)))
@@ -54,14 +21,10 @@ EvtScript N(EVS_Main) = {
     Wait(1)
     Call(SetMusicTrack, 0, SONG_MT_LAVALAVA, 0, 8)
     Call(PlayAmbientSounds, AMBIENT_LAVA_1)
-<<<<<<< HEAD
-    Exec(N(EVS_StartTexPanners))
-=======
     Set(LVar0, MODEL_kem1)
     Exec(N(EVS_StartTexPanner_SmokeLeft))
     Set(LVar0, MODEL_kem2)
     Exec(N(EVS_StartTexPanner_SmokeRight))
->>>>>>> main
     Return
     End
 };
