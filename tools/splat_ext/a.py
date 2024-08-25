00438a19032eccc3a1c757d3c2b426f02066c88e
Alex Bates <alex@bates64.com>
2024-08-25 02:52:24 +0100

    splat 0.27.0

--- tools/splat_ext/a.py
+++ tools/splat_ext/a.py
@@ -1,9 +1,9 @@
-from splat.segtypes.n64.segment import N64Segment
+from splat.segtypes.segment import Segment
 from splat.segtypes.linker_entry import LinkerEntry
 from splat.util import options
 
 
-class N64SegA(N64Segment):
+class N64SegA(Segment):
     def get_linker_entries(self):
         path = options.opts.asset_path / self.dir / (self.name + ".a")
 
