0844bd059332900a65c9e868f5d604134348d09f
Jacob Butler <73204193+Ponmander@users.noreply.github.com>
2024-11-28 00:55:35 -0600

    Update config.h
    Signed-off-by: Jacob Butler <73204193+Ponmander@users.noreply.github.com>

--- src/dx/config.h
+++ src/dx/config.h
@@ -4,7 +4,7 @@
 /// Creates the version string that appears on the file select menu.
 /// Also used to sign save files created with this mod.
 /// WARNING: name must not exceed 27 characters!
-#define DX_MOD_NAME "New Mod"
+#define DX_MOD_NAME "DX"
 #define DX_MOD_VER_MAJOR 1 /// Increase this when you make breaking changes to the savefile format (like reordering items)
 #define DX_MOD_VER_MINOR 0 /// Increase this when adding new features or other enchancements
 #define DX_MOD_VER_PATCH 0 /// Increase this when fixing bugs
