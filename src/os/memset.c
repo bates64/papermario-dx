cb01ae6a6056d61698f432a773e8af7d119255eb
Alex Bates <alex@bates64.com>
2026-03-03 16:51:56 +0000

    implement memmove

--- src/os/memset.c
+++ src/os/memset.c
@@ -15,6 +15,24 @@ void *__memset_chk(void *dest, int val, size_t len, size_t destlen) {
     return __builtin_memset(dest, val, len);
 }
 
+void *memmove(void *dest, const void *src, size_t n) {
+    unsigned char *d = dest;
+    const unsigned char *s = src;
+
+    if (d < s) {
+        while (n--) {
+            *d++ = *s++;
+        }
+    } else {
+        d += n;
+        s += n;
+        while (n--) {
+            *--d = *--s;
+        }
+    }
+    return dest;
+}
+
 void *__memmove_chk(void *dest, const void *src, size_t n, size_t destlen) {
     ASSERT_MSG(n <= destlen, "buffer overflow in bcopy/memmove");
     return __builtin_memmove(dest, src, n);
