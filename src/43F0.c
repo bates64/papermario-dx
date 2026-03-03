bca3e1751b2a19735e4c28cb0147da93d9a82fe9
Alex Bates <alex@bates64.com>
2026-03-03 16:59:13 +0000

    add more asserts to dma_copy

--- src/43F0.c
+++ src/43F0.c
@@ -443,7 +443,9 @@ u32 dma_copy(Addr romStart, Addr romEnd, void* vramDest) {
     u32 length = romEnd - romStart;
     s32 i;
 
-    ASSERT_MSG(((u32)vramDest & 7) == 0, "dma_copy: dest not 8-byte aligned");
+    ASSERT_MSG(((u32)vramDest & 7) == 0, "dest %08x not 8-byte aligned", vramDest);
+    ASSERT_MSG(((u32)romStart & 1) == 0, "romStart %08x not 2-byte aligned", romStart);
+    ASSERT_MSG(((u32)romEnd & 1) == 0, "romEnd %08x not 2-byte aligned", romEnd);
 
     osInvalICache(vramDest, length);
 
