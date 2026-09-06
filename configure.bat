dd05ff952ebb01647921275f07fa2c33d031c901
Darxoon <darxoon@outlook.com>
2026-09-06 21:38:29 +0200

    Split build.bat into build.bat and configure.bat

--- configure.bat
+++ configure.bat
@@ -12,13 +12,6 @@ call tools/windows/download_toolchain.bat
 set "PATH=%TOOLCHAIN_DIR%\bin;%TOOLCHAIN_DIR%\python;%PATH%"
 set "PYTHONUTF8=1"
 
-:: Configure if needed
-if not exist build.ninja (
-    echo Running configure...
-    python.exe tools\build\configure.py
-    if errorlevel 1 exit /b 1
-)
-
-:: Build
-echo Building...
-ninja.exe %*
+:: Configure
+python.exe tools\build\configure.py %*
+if errorlevel 1 exit /b 1
